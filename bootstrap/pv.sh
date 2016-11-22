2a70197869d4d1fc30fd6585829cba881217d8b7
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-11-22 13:49:59 -0800

    Update pv.sh

--- bootstrap/pv.sh
+++ bootstrap/pv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo ""
+echo "Creating persistant volume using hostPath volumes in the current Dir"
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 path="$dir/data"
