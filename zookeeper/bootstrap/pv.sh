43e8ca154e1523670fea95483c4acc563b9ed267
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-11-22 13:48:00 -0800

    Update pv.sh

--- zookeeper/bootstrap/pv.sh
+++ zookeeper/bootstrap/pv.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-echo "Note that in for example GKE a PetSet will have PersistentVolume(s) and PersistentVolumeClaim(s) created for it automatically"
-
+echo "Creating persistant volume using hostPath volumes in the current Dir"
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 path="$dir/data"
-echo "Please enter a path where to store data during local testing: ($path)"
-read newpath
-[ -n "$newpath" ] && path=$newpath
-
 cat zookeeper/bootstrap/pv-template.yml | sed "s|/tmp/k8s-data|$path|" | kubectl create -f -
